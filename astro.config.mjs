d0cf1f1fd9125e333f9db89a3abdfc246e2e9602
Victor <gichuivictor@gmail.com>
2025-03-09 18:42:36 +0300

    feat(setup): add card component to show project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,38 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Works',
-			social: {
-				github: 'https://github.com/Victor-Nyangi',
-			},
-			sidebar: [
-				{
-					label: 'Work',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Work Projects', slug: 'work/example' },
-					],
-				},
-				{
-					label: 'Consultancy',
-					autogenerate: { directory: 'consultancy' },
-				},
-				{
-					label: 'Hobbies',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Hobby Projects', slug: 'hobbies/example' },
-					],				},
-				{
-					label: 'Knowledge',
-					autogenerate: { directory: 'knowledge' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'Victor Gichui\'s Works',
+        social: {
+            github: 'https://github.com/Victor-Nyangi',
+        },
+        sidebar: [
+            {
+                label: 'Work',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Work Projects', slug: 'work/view' },
+                ],
+            },
+            {
+                label: 'Consultancy',
+                autogenerate: { directory: 'consultancy',  slug: 'consultancy/view' },
+            },
+            {
+                label: 'Hobbies',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Hobby Projects', slug: 'hobbies/view' },
+                ],				},
+            {
+                label: 'Knowledge',
+                autogenerate: { directory: 'knowledge' , slug: 'knowledge/view'},
+            },
+        ],
+		}), react(),mdx(),],
 });
